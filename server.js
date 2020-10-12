cd384917a20ab63006052d40bf5a87827d861c97
PlinXXX <plinsy2@gmail.com>
2020-10-12 13:46:47 +0300

    with serve and procfile

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(__dirname + '/dist/front'))
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname + "/dist/front/index.html"))
+})
+
+app.listen(process.env.PORT || 8080);
